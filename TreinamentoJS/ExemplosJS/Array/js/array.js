398a85ed28b68da790f484e426526e7fa1b132d6
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-09-20 00:20:23 -0300

    Adicionando o exemplo de array

---
+++ TreinamentoJS/ExemplosJS/Array/js/array.js
@@ -0,0 +1,12 @@
+const listaDeCompras = ["Açucar", false];  // Aceita todo tipo de dado como String e Booleano
+console.log(listaDeCompras);
+
+listaDeCompras[0] = "Arroz";
+listaDeCompras[1] = "Feijão";
+listaDeCompras[2] = 7;
+listaDeCompras[5] = "Batata";
+
+
+console.log(listaDeCompras); // Mostra todo o array
+
+console.log(listaDeCompras[4]); // A posição dp array de número 4 está nula 
