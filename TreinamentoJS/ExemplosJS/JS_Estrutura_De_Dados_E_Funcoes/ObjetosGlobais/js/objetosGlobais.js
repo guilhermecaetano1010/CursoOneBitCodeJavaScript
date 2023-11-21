6aa249ad99791a45effe9ac157be21112029f3e2
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-11-21 17:06:05 -0300

    Adicionando exemplo de Objetos Globais

---
+++ TreinamentoJS/ExemplosJS/JS_Estrutura_De_Dados_E_Funcoes/ObjetosGlobais/js/objetosGlobais.js
@@ -0,0 +1,54 @@
+let pessoa = Object()
+
+console.log(pessoa)
+
+let lista = Array()
+
+console.log(lista)
+
+lista = Array("A", "B", "C")
+
+console.log(lista)
+
+let a = String()
+
+console.log(a)
+
+a = String(42)
+
+console.log(a)
+
+a = String(true)
+
+console.log(a)
+
+a = String(false)
+console.log(a)
+
+
+num = Number("42")
+
+console.log(num)
+
+num = Number.isInteger(4.5)
+
+console.log(num)
+
+num = Number.isInteger(4)
+
+console.log(num)
+
+
+date = Date()
+
+console.log(date)
+
+math = Math.PI
+console.log(math)
+
+math = Math.pow(2, 10)
+console.log(math)
+
+math = Math.sqrt(144)
+console.log(math)
+
