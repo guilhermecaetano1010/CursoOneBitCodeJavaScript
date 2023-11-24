33934537692d00717fc9c28e0d9b60acc94939bb
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-11-24 11:21:38 -0300

    Criando a pasta de JS DOM e colocando o exemplo de eventos no HTML

---
+++ TreinamentoJS/ExemplosJS/JS_DOM/eventosHtml/js/index.js
@@ -0,0 +1,15 @@
+function onMouseEnter() {
+    console.log('mouse enter')
+}
+function onChange() {
+    console.log('change')
+}
+function onFocus() {
+    console.log('focus')
+}
+function onBlur() {
+    console.log('blur')
+}
+function onClick() {
+    console.log('click')
+}
