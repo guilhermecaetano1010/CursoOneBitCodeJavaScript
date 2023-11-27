d710147f9bae14c2a932233b295c305442d03224
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-11-27 10:25:52 -0300

    Modificando o exemplo de praticando a manipulação do DO

--- TreinamentoJS/ExerciciosJS/Exercicio11/js/escalacaoTime.js
+++ TreinamentoJS/ExerciciosJS/Exercicio11/js/escalacaoTime.js
@@ -1,10 +1,3 @@
-function adicionarJogador() {
-    const playerSection = document.getElementById("players-list")
-     
-    const h3 = document.createElement('h3')
-    h3.innerText('Jogador')
-
-
-
-
+function addPlayer() {
+    
 }
