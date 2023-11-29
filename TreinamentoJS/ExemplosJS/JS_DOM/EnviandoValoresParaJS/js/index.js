2f6c629597ed0d89ace308127f56f96c7c36284a
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-11-29 10:32:19 -0300

    Adicionando o exemplo enviando valores do DOM para o Java Script

---
+++ TreinamentoJS/ExemplosJS/JS_DOM/EnviandoValoresParaJS/js/index.js
@@ -0,0 +1,14 @@
+function register(element){
+//console.log(element)
+const username = element.children.username.value
+const password = element.children.password.value
+const passwordConfirmation = element.children.passwordConfirmation.value
+
+// console.log({username, password, passwordConfirmation})
+
+if (password === passwordConfirmation) {
+    alert("Usuário " + username + " registrado!")
+} else {
+    alert("As senhas não conferem!")
+}
+}
