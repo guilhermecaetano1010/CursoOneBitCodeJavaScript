4be367c512602d313a27747b18f041c3d9a4135e
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-11-29 17:06:28 -0300

    Alterando o exemplo trabalhando com formulários

--- TreinamentoJS/ExemplosJS/JS_DOM/TrabalhandoComFormularios/js/index.js
+++ TreinamentoJS/ExemplosJS/JS_DOM/TrabalhandoComFormularios/js/index.js
@@ -4,8 +4,8 @@ form.addEventListener('submit', function(ev){
     ev.preventDefault()
 
 
-    const name = document.querySelector('input[name="name"').value
-    const address = document.querySelector('input[name="address"').value
+    const name = document.querySelector('input[name="name"]').value
+    const address = document.querySelector('input[name="address"]').value
     const breadType = document.querySelector('select[name="breadType"]').value
     const main = document.querySelector('input[name="main"]').value
     const observations = document.querySelector('textarea[name="observations"]').value
